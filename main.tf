27bd9a1cf3a0058add193528f3fedf8bc560a238
Benoit Blais <benoit.blais@hashicorp.com>
2025-10-31 16:13:33 -0400

    project_id

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-resource "hcp_waypoint_tfc_config" "test" {
+resource "hcp_waypoint_tfc_config" "this" {
   token        = var.team_token
   tfc_org_name = "benoit.blais-hashicorp"
+  project_id   = "8fcdc510-5fd7-40d0-8094-96c523943529"
 }
