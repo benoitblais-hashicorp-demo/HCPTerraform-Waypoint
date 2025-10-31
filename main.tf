caf898f06dea60a21c12987482461ea8ec53e2f7
Benoit Blais <benoit.blais@hashicorp.com>
2025-10-31 16:17:41 -0400

    change org name

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "hcp_waypoint_tfc_config" "this" {
   token        = var.team_token
-  tfc_org_name = "benoit.blais-hashicorp"
-  project_id   = "8fcdc510-5fd7-40d0-8094-96c523943529"
+  tfc_org_name = "benoitblais-hashicorp"
 }
